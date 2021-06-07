5ca6d8ab7d5e7326042507e2160ad1e8d7158abd
JeongeunJPark <jeongeunswd@gmail.com>
2021-06-07 13:42:36 +0900

    First Commit +0607

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ssac23/ros_input/src
+# Build directory: /home/ssac23/ros_input/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("inputs")
---
+++ build/inputs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ssac23/ros_input/src/inputs
+# Build directory: /home/ssac23/ros_input/build/inputs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
